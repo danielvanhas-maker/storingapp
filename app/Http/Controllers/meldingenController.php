ef5947e884a6476530c53333d52b6a93c324d007
danielvanhassel <danielvanhassel@ziggo.nl>
2026-04-16 14:16:44 +0200

    fixed meldingen index

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -1,6 +1,7 @@
 <?php
+$action = $_POST['action'];
+if ($action == 'create'){
 
-//Variabelen vullen
 $attractie = $_POST['attractie'];
 $type = $_POST['type'];
 $capaciteit = $_POST['capaciteit'];
@@ -10,18 +11,24 @@ $overige_info = $_POST['overige_info'];
 
 echo $attractie . " / " . $type . " / " . $capaciteit . " / " . $prioriteit . " / " . $melder . " / " . $overige_info;
 
-//1. Verbinding
 require_once '../../../config/conn.php';
-
-//2. Query
 $query = "INSERT INTO meldingen (attractie, type, capaciteit, prioriteit, melder, overige_info)
             VALUE (:attractie, :type, :capaciteit, :prioriteit, :melder, :overige_info)";
-
-//3. Prepare
 $statement = $conn->prepare($query);
-
-//4. Execute
 $statement->execute([":attractie" => $attractie, ":type" => $type, ":capaciteit" => $capaciteit,
     ":prioriteit" => $prioriteit, ":melder" => $melder, ":overige_info" => $overige_info,]);
 
 header("Location: ../../../index.php");
+}
+
+if ($action == 'edit'){
+    $id = (int) $_POST['id'];
+    $name = $_POST['name'];
+    $password = $_POST['password'];
+
+    require_once "conn.php"; 
+    $query = "UPDATE user SET name = :name, password = :password WHERE id = :id";
+    $statement = $conn->prepare($query);
+    $statement->execute([":name" => $name, ":password" => $password, ":id" => $id]);
+    header("Location: ../../../meldingen/index.php");
+}
--- resources/views/meldingen/index.php
+++ resources/views/meldingen/index.php
@@ -1,4 +1,4 @@
-<?php require_once __DIR__.'/../../../config/config.php'; ?>
+<?php require_once __DIR__.'/../../../config/conn.php'; ?>
 <!doctype html>
 <html lang="nl">
 <?php session_start(); ?>
@@ -15,20 +15,54 @@
     <?php if (!isset($_SESSION['logged']))
     {
         header("Location: ../../../index.php");
+        exit;
     }
+
+    $query = "SELECT * FROM meldingen";
+    $statement = $conn->prepare($query);
+    $statement->execute([]);
+    $meldingen = $statement->fetchAll(PDO::FETCH_ASSOC);
     ?>
 
     <div class="container">
         <h1>Meldingen</h1>
         <a href="create.php">Nieuwe melding &gt;</a>
 
+        <div class="row">
+
+<div class="taskContainer">
+    <table>
+        <tr>
+            <th>attractie</th>
+            <th>type</th>
+            <th>capaciteit</th>
+            <th>prioriteit</th>
+            <th>melder</th>
+            <th>gemeld_op</th>
+            <th>overige_info</th>
+            <th colspan="1">Acties</th>
+        </tr>
+
+    <?php foreach ($meldingen as $melding): ?>
+        <tr>
+            <td><?= htmlspecialchars($melding['attractie']) ?></td>
+            <td><?= htmlspecialchars($melding['type']) ?></td>
+            <td><?= htmlspecialchars($melding['capaciteit']) ?></td>
+            <td><?= htmlspecialchars($melding['prioriteit']) ?></td>
+            <td><?= htmlspecialchars($melding['melder']) ?></td>
+            <td><?= htmlspecialchars($melding['gemeld_op']) ?></td>
+            <td><?= htmlspecialchars($melding['overige_info']) ?></td>
+            <td class="Actions"><a href="edit.php?id=<?= $melding['id']; ?>" class="edit">Edit</a></td>
+        </tr>
+    <?php endforeach ?>
+    </table>
+</div>
+</div>
         <?php if(isset($_GET['msg']))
         {
             echo "<div class='msg'>" . $_GET['msg'] . "</div>";
         } ?>
-
-        <div style="height: 300px; background: #ededed; display: flex; justify-content: center; align-items: center; color: #666666;">(hier komen de storingsmeldingen)</div>
-    </div>
+</div>
 
 </body>
 
